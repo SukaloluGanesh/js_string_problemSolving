e9624344f5bff17b724e95d94141a4ef551caed4
SukaloluGanesh <147176578+SukaloluGanesh@users.noreply.github.com>
2024-11-21 16:58:58 +0530

    Strings problem solving

---
+++ index.js
@@ -0,0 +1,108 @@
+
+// 1.Reverse a String: Input: 'hello' | Output: 'olleh'. 
+
+// let str = "hello";
+// reverse = "";
+// for(let i =str.length-1;i>=0;i--){
+//  reverse += str[i];
+// }
+// console.log(reverse)
+
+// 2.Check Palindrome: Input: 'radar' | Output: True .
+
+// let str =  "radar"
+// reverse = "";
+// for(let i = str.length-1;i>=0;i--){
+//   reverse += str[i];
+// }
+
+// if(str === reverse){
+//   console.log(true)
+// }else{
+//   console.log(false)
+// }
+
+// 3.Find Length: Input: 'hello' | Output: 5.
+
+// let str = "hello";
+// let length = str.length;
+// console.log(length)
+
+
+// 4.Count Vowels and Consonants: Input: 'hello' | Output: Vowels: 2, Consonants: 3 .
+
+// let n = "hello";
+// let vowels = 0;
+// let Consonants = 0;
+// for(let i = 0;i<n.length;i++){
+//   if(n[i]==="a"||n[i]==="i"||n[i]==="e"||n[i]==="o"||n[i]==="u"){
+//     vowels++;
+//   }else{
+//     Consonants++;
+//   }
+// }
+// console.log(vowels);
+// console.log(Consonants);
+
+// 5.Change Case: Input: 'Hello' | Output: 'hELLO' .
+
+// let n = "Hello";
+// let v = "";
+// for(let i =0;i<n.length;i++){
+//   if(n[i]===n[i].toUpperCase()){
+//      v = v + n[i].toLowerCase()
+//   }else{
+//     v = v + n[i].toUpperCase();
+//   }
+// }
+// console.log(v)
+
+// 6.Remove White Spaces: Input: 'hello world' | Output: 'helloworld' 
+
+// let n = "hello world"
+// let s = "";
+// for(let i =0;i<n.length;i++){
+//   if(n[i]===" "){
+//     continue;
+//   }else{
+//     s = s + n[i];
+//   }
+// }
+// console.log(s);
+
+// 7.Concatenate Strings: Input: 'hello', 'world' | Output: 'helloworld'.
+
+// let f = "hello";
+// let l = "world";
+// console.log(f+l);
+
+// 8.Compare Two Strings: Input: 'abc', 'abc' | Output: True .
+
+// let a = "abc";
+// let b = "abc";
+
+// if(a === b){
+//   console.log(true)
+// }else{
+//   console.log(false)
+// }
+
+// 9.Repeat a String: Input: 'abc', 3 | Output: 'abcabcabc' .
+
+// let str = "abc";
+
+// let Repeat = str.repeat(3)
+// console.log(Repeat)
+
+// 10. Replace a Character: Input: 'hello', 'l', 'z' | Output: 'hezzo'.
+
+// let n = "hello"
+// let o = "";
+// for(let i= 0;i<n.length;i++){
+//   if(n[i]==="l"){
+//     o += "z"
+//   }else{
+//     o += n[i]
+//   }
+// }
+// console.log(o)
